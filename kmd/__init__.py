67000faf212554403955fbc7630d2ca205d06451
Stefan H. Holek <stefan@epy.co.at>
2011-08-03 15:42:49 +0200

    Provide initial package docstring.

--- kmd/__init__.py
+++ kmd/__init__.py
@@ -1,1 +1,16 @@
+"""A modern version of cmd.Cmd using rl readline bindings.
+
+Package Contents
+================
+
+kmd exports these components:
+
+`kmd.Kmd`
+    Implements the mechanics of a command shell. Used as a base class
+    for custom command interpreters.
+
+`kmd.completions`
+    Implements a set of ready-to-use completions.
+"""
+
 from kmd import Kmd
