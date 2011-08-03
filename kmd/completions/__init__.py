0c9fa20f7e013fbdcae0f9aa870361c7c6afdfc4
Stefan H. Holek <stefan@epy.co.at>
2011-08-03 21:40:37 +0200

    Add overview in completions package docstring.

--- kmd/completions/__init__.py
+++ kmd/completions/__init__.py
@@ -1,1 +1,20 @@
-"""A set of completions for use with kmd.Kmd."""
+"""A set of completions for use with kmd.Kmd.
+
+Package Contents
+================
+
+`CommandCompletion`
+    Complete system commands.
+
+`EnvironmentCompletion`
+    Complete environment variables.
+
+`FilenameCompletion`
+    Complete file and directory names.
+
+`HostnameCompletion`
+    Complete host names.
+
+`UsernameCompletion`
+    Complete user names.
+"""
