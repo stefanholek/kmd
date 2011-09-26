cd83d06abf369ba140adeabe718a85cbfba1dd21
Stefan H. Holek <stefan@epy.co.at>
2011-09-26 20:31:27 +0200

    Line wrapping.

--- kmd/__init__.py
+++ kmd/__init__.py
@@ -6,9 +6,8 @@ Command Interpreters
 ====================
 
 The :class:`kmd.Kmd` class provides a simple framework for writing
-line-oriented command
-interpreters, also known as *shells*. These are often useful for test harnesses,
-prototypes, and administrative tools.
+line-oriented command interpreters, also known as *shells*. These are often
+useful for test harnesses, prototypes, and administrative tools.
 The main UI feature of shell-type applications is TAB completion.
 
 A Kmd instance is a line-oriented interpreter framework.
@@ -18,9 +17,12 @@ base class for interpreter classes you define.
 Custom Completions
 ==================
 
-The :mod:`kmd.completions` package implements all OS-related completions known from
-Bash. In addition, it establishes the *custom completion protocol* and
+The :mod:`kmd.completions` package implements all OS-related completions known
+from Bash.  In addition, it establishes the *custom completion protocol* and
 provides utility functions for string and filename quoting.
+
+Applications are encouraged to create their own, domain-specific
+completions based on the code in this package.
 """
 
 from kmd import Kmd
