65a5f8549645921d1ad79ba76201eb7016ca797b
Stefan H. Holek <stefan@epy.co.at>
2011-10-06 18:38:57 +0200

    Prepare kmd 2.0.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '2.0'
 
 
 setup(name='kmd',
@@ -29,6 +29,6 @@ setup(name='kmd',
       test_suite='kmd.tests',
       install_requires=[
           'setuptools',
-          'rl > 1.16',
+          'rl >= 2.0',
       ],
 )
