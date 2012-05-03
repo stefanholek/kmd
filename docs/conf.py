6fa15c3f5f740cf1306e3ecf503d67a0e686151e
Stefan H. Holek <stefan@epy.co.at>
2012-05-03 15:01:21 +0200

    Make branch for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -91,8 +91,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-html_theme = 'sphinxdoc'
+if os.environ.get('READTHEDOCS'):
+    html_theme = 'default'
+else:
+    html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
